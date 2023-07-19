5c03a19581f1825359f923f653ac8fb743a520a4
Cole Lawrence <cole@colelawrence.com>
2023-07-19 20:21:51 -0300

    Update token lookup to also include the required tokens

--- extensions/Here Now Figma/gen/TypographyTokenLookup.ts
+++ extensions/Here Now Figma/gen/TypographyTokenLookup.ts
@@ -1,14 +1,19 @@
 import * as gen from "./figma-typography-export.gen.js";
 
+export type TokenQueryOutput = {
+  properties: gen.TypographyProperty[];
+  requiredTokens: Set<string>;
+};
+
 // Maybe more of this logic should be in Rust or in Figma?
 // Hard to say since we also want to support custom combinations in
 // Figma plugin, which require this querying logic to be executed in
 // the plugin itself.
 export class TypographyTokenLookup {
   constructor(private allTokens: gen.TypographyExport) {}
-  query(tokens: string[]): gen.TypographyProperty[] {
+  query(tokens: string[]): TokenQueryOutput {
     // precedence + props
-    const found: [number, number[]][] = [];
+    const found: [number, { propIdxs: number[]; reqs: string[] }][] = [];
     possible: for (const [reqs, propIdxs] of this.allTokens.tokens) {
       let precedence = -1;
       for (const req of reqs) {
@@ -20,17 +25,25 @@ export class TypographyTokenLookup {
       }
 
       // matched
-      found.push([precedence, propIdxs]);
+      found.push([precedence, { propIdxs, reqs }]);
     }
 
+    // todo: might need to collect all unique tokens
     const allProps: gen.TypographyProperty[] = [];
+    const allTokens = new Set<string>();
     const byPrecedence = found.sort((a, b) => a[0] - b[0]);
-    for (const [_, idxs] of byPrecedence) {
-      for (const idx of idxs) {
+    for (const [_, { propIdxs, reqs }] of byPrecedence) {
+      for (const idx of propIdxs) {
         allProps.push(this.allTokens.properties[idx]);
       }
+      for (const token of reqs) {
+        allTokens.add(token);
+      }
     }
-    return allProps;
+    return {
+      properties: allProps,
+      requiredTokens: allTokens,
+    };
   }
 }
 
